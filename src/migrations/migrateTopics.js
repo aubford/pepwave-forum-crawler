888fa3f5d1383fce9d58cf11089e20f3a5ec9b1e
Aubrey <MusicAubrey@gmail.com>
2024-05-27 17:14:32 -0700

    fixes

---
+++ src/migrations/migrateTopics.js
@@ -0,0 +1,30 @@
+import { runMongoSession } from "../mongo.js"
+
+const main = async db => {
+  const topicsCollection = db.collection("topics")
+  const cursor = topicsCollection.find({})
+  while (await cursor.hasNext()) {
+    const doc = await cursor.next()
+
+    await topicsCollection.updateOne(
+      { _id: doc._id },
+      {
+        $set: { _id: doc.id },
+        $unset: { id: "" },
+      }
+    )
+  }
+  while (await cursor.hasNext()) {
+    const doc = await cursor.next()
+
+    await topicsCollection.updateOne(
+      { _id: doc._id },
+      {
+        $set: { _id: doc.id },
+        $unset: { id: "" },
+      }
+    )
+  }
+}
+
+runMongoSession(main).then(() => console.log("Done!"))
